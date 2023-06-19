a7ec28c115167cc28589baa1d3860a80bf5e55d2
lawsssscat <lawsssscat@163.com>
2023-06-19 18:46:08 +0800

    [CLP] improve tracker fetching error message readability

--- tasks/tasks/add_trackers_update.sh
+++ tasks/tasks/add_trackers_update.sh
@@ -13,8 +13,14 @@ tracker_old=$(echo "$qbt_app_preferences" | $jq_executable ".add_trackers" -r) |
 
 # fetch
 fetch_urls=($qbt_tracker_fetch_urls) && 
-fetch_net_trackers "${fetch_urls[@]}" && 
-tracker_fetch="$(echo "$qbt_net_trackers")" || exit 1 # qbt_net_trackers
+fetch_net_trackers "${fetch_urls[@]}" || exit 1 # qbt_net_trackers
+tracker_fetch="$(echo "$qbt_net_trackers")" 
+# fetch error message
+tracker_fetch_error=""
+for ((i=0; i<${#qbt_net_exception_urls[@]}; i++)); do
+    tracker_fetch_error+="Fail to fetch \"${qbt_net_exception_urls[$i]}\" with issue: ${qbt_net_exception_issues[$i]}"
+    tracker_fetch_error+=$'\n'
+done
 
 # update
 set_app_preferences "{\"add_trackers\":\"$(echo "$qbt_net_trackers" | sed ':a; N; $!ba; s/\n/\\n/g')\"}" || exit 1
@@ -23,5 +29,9 @@ set_app_preferences "{\"add_trackers\":\"$(echo "$qbt_net_trackers" | sed ':a; N
 get_app_preferences && 
 tracker_new=$(echo "$qbt_app_preferences" | $jq_executable ".add_trackers" -r) || exit 1
 
-# print
+# print update stats
 echo "tracker update: OLD,FETCH,NEW=$(lines_number "$tracker_old"),$(lines_number "$tracker_fetch"),$(lines_number "$tracker_new")"
+# print fetch error
+if [ -n "$tracker_fetch_error" ]; then
+    echo "$(lines_trim "$tracker_fetch_error")" >&2
+fi
