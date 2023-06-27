b5adfc4f45cca19be4eb3505d9682ae13e216754
lawsssscat <lawsssscat@163.com>
2023-06-27 22:54:03 +0800

    perf(ban_peers_add): better error message output

--- tasks/tasks/ban_peers_add.sh
+++ tasks/tasks/ban_peers_add.sh
@@ -24,34 +24,45 @@ function get_matching_peers {
     }
     # peers
     local tmp_mached_peers=""
+    local tmp_error_count=0
     for h in $qbt_torrent_hashs; do 
         get_torrent_peers "$h" || {
             task_message_push "fail get torrent: $h <--> [$qbt_webapi_response_status] $qbt_webapi_response_error"
             continue
         }
+        local tmp_get_torrent_peers_raw="$qbt_webapi_response_body"
         local tmp_get_peers=""
-        tmp_get_peers="$(echo "$qbt_webapi_response_body" | $jq_executable ".peers | to_entries[]" -c 2>&1)" || {
-            task_message_push "fail parse torrent: $h <--> $tmp_get_peers"
+        tmp_get_peers="$(echo "$tmp_get_torrent_peers_raw" | $jq_executable ".peers | to_entries[]" -c 2>&1)" || {
+            ((tmp_error_count++))
+            task_message_push "fail parse torrent-1: $h <--> $tmp_get_peers"
+            task_message_push "raw torrent: $tmp_get_torrent_peers_raw"
             continue
         }
         tmp_get_peers="$(echo "$tmp_get_peers" | grep -E "$pattern_peer")" || {
             continue
         }
+        tmp_get_peers="$(echo "$tmp_get_peers" | $jq_executable ".key" -r 2>&1)" || {
+            ((tmp_error_count++))
+            task_message_push "fail parse torrent-2: $h <--> $tmp_get_peers"
+            task_message_push "raw torrent: $tmp_get_torrent_peers_raw"
+            continue
+        }
         tmp_mached_peers+="$tmp_get_peers"
         tmp_mached_peers+=$'\n'
     done
+    qbt_mached_error_count="$tmp_error_count"
     qbt_mached_peers="$(echo "$tmp_mached_peers" | lines_trim | lines_unique)" 
 }
 get_matching_peers "$qbt_peer_ban_pattern"
-qbt_banned_IPs_add="$(echo "$qbt_mached_peers" | $jq_executable ".key" -r 2>&1)" || {
-    task_title_push "fail parse torrent: $qbt_banned_IPs_add"
-    task_message_push "$qbt_mached_peers"
-    task_fatal
-}
+qbt_banned_IPs_add="$qbt_mached_peers"
+if [ -z "$qbt_banned_IPs_add" ] && [ $qbt_mached_error_count -gt 0 ]; then
+        task_message_push "No matching peers, but errors"
+        task_fatal
+fi
 
 # 2. add banned_IPs
-if [ -z "$qbt_banned_IPs_add" ]; then
-    task_skip
+if [ -z "$qbt_banned_IPs_add" ] && [ $qbt_mached_error_count -eq 0 ]; then
+        task_skip
 fi
 qbt_banned_IPs_add_params="$(lines_join '|' "$qbt_banned_IPs_add")"
 debug "banned_IPs: $qbt_banned_IPs_add_params"
