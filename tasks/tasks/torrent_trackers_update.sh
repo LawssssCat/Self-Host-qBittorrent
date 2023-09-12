1be80b9d0049a55cb2a7194220272e7d667920a6
lawsssscat <lawsssscat@163.com>
2023-09-12 13:02:13 +0800

    Add pt torrent filtering when updating torrent trackers

--- tasks/tasks/torrent_trackers_update.sh
+++ tasks/tasks/torrent_trackers_update.sh
@@ -5,6 +5,7 @@
 
 : ${qbt_trackers_update_torrent_num:=10}
 : ${qbt_torrent_pt_tag:=pt}
+: ${qbt_torrent_pt_category:=pt}
 
 # Source library of functions
 source /qbittorrent-web-api-tools/lib/qb.shlib
@@ -38,6 +39,7 @@ debug "active_torrents: $qbt_webapi_response_body"
 qbt_active_torrents="$( (echo "$qbt_webapi_response_body" \
     | $jq_executable 'sort_by(.progress) | sort_by(.upspeed) | sort_by(.dlspeed) | .[]' -c \
     | grep -E -v '\"tags\":\"([^"]*, |)?'$qbt_torrent_pt_tag'(, [^"]*|)?\"' \
+    | grep -E -v '\"category\":\"'$qbt_torrent_pt_category'\"' \
     | head -n $qbt_trackers_update_torrent_num) 2>&1)" || {
         task_title_push "fail parse json: $qbt_active_torrents"
         task_fatal
